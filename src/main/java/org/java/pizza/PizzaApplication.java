8387c89d89f2b9c8fc9399076ca2bb2c84d4420c
Ali-Chouchene <choucheneali9801@gmail.com>
2023-05-28 16:57:00 +0200

    ended

--- src/main/java/org/java/pizza/PizzaApplication.java
+++ src/main/java/org/java/pizza/PizzaApplication.java
@@ -32,17 +32,22 @@ public class PizzaApplication implements CommandLineRunner {
 
 		pizzas.add(new Pizza("Americana", "Pomodoro, Mozzarella, Wurstel",
 				"https://external-preview.redd.it/bnDRcRxxhrKkypxeiM8zlAn7J1nfeo-YnIIE2uPAAOY.jpg?width=640&crop=smart&auto=webp&s=cb80465e46dac28e4b2adb8ba652371376b600b2",
-				2.5));
+				5.5));
 		pizzas.add(new Pizza("Margherita", "Pomodoro, Mozzarella, Patatine",
 				"https://www.lemilleeunabontadifranci.it/wp-content/uploads/2021/04/Pizza-margherita-fatta-in-casa-orizzontale.jpg",
-				1.50));
+				5.50));
 		pizzas.add(new Pizza("Napoli", "Pomodoro, Mozzarella, Acciughe, Capperi",
 				"https://www.donnamoderna.com/content/uploads/2021/08/pizza-napoli-830x625.jpg", 1.50));
 		pizzas.add(new Pizza("4 Stagioni", "Pomodoro, Mozzarella, Carciofi, Olive, Cotto, Funghi",
-				"https://cdn.cook.stbm.it/thumbnails/ricette/144/144880/hd750x421.jpg", 1.50));
+				"https://cdn.cook.stbm.it/thumbnails/ricette/144/144880/hd750x421.jpg", 7.50));
 		pizzas.add(new Pizza("Salame Piccante", "Pomodoro, Mozzarella, Salame Piccante",
 				"https://media.istockphoto.com/id/1142744772/it/foto/pizza-pepperoni-su-sfondo-di-cemento-nero.jpg?s=612x612&w=0&k=20&c=4OHrtlThkiICrHd9JSbhG1E52U-GS7am1VI_ts106_0=",
-				1.50));
+				8.00));
+		pizzas.add(new Pizza("Speck e Bufala", "Pomodoro, Mozzarella, Speck, Bufala",
+				"https://www.50toppizza.it/wp-content/uploads/2022/05/Foscolo-Tre.jpg", 7.50));
+		pizzas.add(new Pizza("Calzone", "Pomodoro, Mozzarella, Basilico",
+				"https://blog.giallozafferano.it/giovannibrigandi69/wp-content/uploads/2019/07/FFC4F4F6-5036-4099-985E-335FB0D4EAEA.jpeg",
+				7.50));
 		return pizzas;
 	}
 
--- src/main/java/org/java/pizza/controller/PizzaController.java
+++ src/main/java/org/java/pizza/controller/PizzaController.java
@@ -1,6 +1,7 @@
 package org.java.pizza.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.java.pizza.Pizza;
 import org.java.pizza.serv.PizzaService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,9 +22,18 @@ public class PizzaController {
 	public String pizzas(Model model) {
 
 		List<Pizza> pizzas = pizzaService.findAll();
-		System.out.println(pizzas);
 		model.addAttribute("pizzas", pizzas);
 		return "index";
+
+	}
+
+	@GetMapping("/pizza/{id}")
+	public String pizzaId(Model model, @PathVariable("id") int id) {
+		Optional<Pizza> pizzaId = pizzaService.findById(id);
+		Pizza pizza = pizzaId.get();
+		System.out.println(pizza);
+		model.addAttribute("pizza", pizza);
+		return "single";
 
 	}
 
